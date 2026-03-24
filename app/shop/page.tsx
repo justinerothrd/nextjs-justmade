2c3d0a567a97514b8c8c68ec5cffa6f19b3c1a2e
justinerothrd <justinerothrd@gmail.com>
2026-03-24 18:24:41 -0400

    Update page.tsx

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,21 +1,33 @@
 const products = [
   {
-    slug: "quarter-zip",
-    name: "Custom 1/4 Zip",
-    price: "$58",
-    image: "/sweatshirt.jpg",
-  },
-  {
     slug: "tank-top",
     name: "Custom Tank Top",
     price: "$32",
-    image: "/tee.jpg",
+    image: "/tank.jpg",
+  },
+  {
+    slug: "hoodie",
+    name: "Custom Hoodie",
+    price: "$60",
+    image: "/hoodie.jpg",
+  },
+  {
+    slug: "quarter-zip",
+    name: "Custom 1/4 Zip",
+    price: "$58",
+    image: "/quarterzip.jpg",
   },
   {
     slug: "custom-shorts",
     name: "Custom Shorts",
     price: "$36",
     image: "/shorts.jpg",
+  },
+  {
+    slug: "custom-tee",
+    name: "Custom Tee",
+    price: "$28",
+    image: "/tee.jpg",
   },
 ];
 
