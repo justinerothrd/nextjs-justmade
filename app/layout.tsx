972ab6bc9473932de395e20b72c816d518a3d669
justinerothrd <justinerothrd@gmail.com>
2026-03-25 16:33:09 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
               <img
                 src="/logo.png"
                 alt="Just Made Custom logo"
-                className="h-22 w-auto"
+                className="h-26 w-auto"
               />
             </a>
 
