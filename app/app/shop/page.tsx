4a6f671bac94efd50869fe726f056e6ed0d3ba99
justinerothrd <justinerothrd@gmail.com>
2026-03-25 12:59:17 -0400

    Create page.tsx

---
+++ app/app/shop/page.tsx
@@ -0,0 +1,8 @@
+export default function ShopPage() {
+  return (
+    <main className="p-10">
+      <h1 className="text-3xl">Shop</h1>
+      <p className="mt-4">This is the shop page.</p>
+    </main>
+  );
+}
