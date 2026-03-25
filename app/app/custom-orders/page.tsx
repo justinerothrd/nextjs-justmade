4fe6f67a12481f109826a9a8a0b070d87bdc115e
justinerothrd <justinerothrd@gmail.com>
2026-03-25 13:00:17 -0400

    Delete app/app directory

--- app/app/custom-orders/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function CustomOrdersPage() {
-  return (
-    <main className="p-10">
-      <h1 className="text-3xl">Custom Orders</h1>
-      <p className="mt-4">This is the custom orders page.</p>
-    </main>
-  );
-}
