1c2373b3a91caa22d18a4d61b4292d51c5b0588a
justinerothrd <justinerothrd@gmail.com>
2026-04-19 02:24:33 -0400

    Update page.tsx

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,36 +1,33 @@
+import { products } from "@/lib/products";
 export default function ShopPage() {
   const categories = [
-    {
-      slug: "sweatshirts",
-      title: "Sweatshirts",
-      image: "/hoodie.jpeg",
-      link: "/shop/sweatshirts",
-    },
-    {
-      slug: "tees",
-      title: "Tees & Tanks",
-      image: "/Tank.jpeg",
-      link: "/shop/tees",
-    },
-    {
-      slug: "bottoms",
-      title: "Bottoms",
-      image: "/bottoms.jpg",
-      link: "/shop/bottoms",
-    },
-    {
-      slug: "sleepwear",
-      title: "Sleep & Loungewear",
-      image: "/sleepwear.jpg",
-      link: "/shop/sleepwear",
-    },
-    {
-      slug: "accessories",
-      title: "Accessories & Gifts",
-      image: "/accessories.jpg",
-      link: "/shop/accessories",
-    },
-  ];
+  {
+    slug: "sweatshirts",
+    title: "Sweatshirts",
+    image: products["hoodie"].images[0],
+  },
+  {
+    slug: "tees",
+    title: "Tees & Tanks",
+    image: products["tank-top"].images[0],
+  },
+  {
+    slug: "bottoms",
+    title: "Bottoms",
+    image: products["custom-shorts"].images[0],
+  },
+  {
+    slug: "sleepwear",
+    title: "Sleep & Loungewear",
+    image: products["sleepwear"].images[0],
+  },
+  {
+    slug: "accessories",
+    title: "Accessories & Gifts",
+    image: products["accessories-slides"].images[0],
+  },
+];
+    
 
   return (
     <main className="bg-white text-[#4B4B4B]">
