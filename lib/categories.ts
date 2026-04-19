a3e5ece1a1a8331a1bd516a00e111e73d568941a
justinerothrd <justinerothrd@gmail.com>
2026-04-19 14:33:24 -0400

    Create categories.ts

---
+++ lib/categories.ts
@@ -0,0 +1,27 @@
+export const categories = [
+  {
+    slug: "sweatshirts",
+    title: "Sweatshirts",
+    image: "/hoodie.jpeg",
+  },
+  {
+    slug: "tees",
+    title: "Tees & Tanks",
+    image: "/WM81tank.png",
+  },
+  {
+    slug: "bottoms",
+    title: "Bottoms",
+    image: "/PCsweatpants.jpg", // 👈 ONLY CHANGE HERE EVER
+  },
+  {
+    slug: "sleepwear",
+    title: "Sleep & Loungewear",
+    image: "/sleepwear.jpg",
+  },
+  {
+    slug: "accessories",
+    title: "Accessories & Gifts",
+    image: "/accessories.jpg",
+  },
+];
