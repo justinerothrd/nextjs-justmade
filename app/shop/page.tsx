bbf1f3932a780755ffa07e13ed26524330789f33
justinerothrd <justinerothrd@gmail.com>
2026-04-19 14:27:40 -0400

    Update page.tsx

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,33 +1,31 @@
-import { products } from "@/lib/products";
 export default function ShopPage() {
   const categories = [
-  {
-    slug: "sweatshirts",
-    title: "Sweatshirts",
-    image: products["hoodie"].images[0],
-  },
-  {
-    slug: "tees",
-    title: "Tees & Tanks",
-    image: products["tank-top"].images[0],
-  },
-  {
-    slug: "bottoms",
-    title: "Bottoms",
-    image: products["custom-shorts"].images[0],
-  },
-  {
-    slug: "sleepwear",
-    title: "Sleep & Loungewear",
-    image: products["sleepwear"].images[0],
-  },
-  {
-    slug: "accessories",
-    title: "Accessories & Gifts",
-    image: products["accessories-slides"].images[0],
-  },
-];
-    
+    {
+      slug: "sweatshirts",
+      title: "Sweatshirts",
+      image: "/hoodie.jpeg",
+    },
+    {
+      slug: "tees",
+      title: "Tees & Tanks",
+      image: "/Tank.jpeg",
+    },
+    {
+      slug: "bottoms",
+      title: "Bottoms",
+      image: "/PCsweatpants.jpg",
+    },
+    {
+      slug: "sleepwear",
+      title: "Sleep & Loungewear",
+      image: "/sleepwear.jpg",
+    },
+    {
+      slug: "accessories",
+      title: "Accessories & Gifts",
+      image: "/accessories.jpg",
+    },
+  ];
 
   return (
     <main className="bg-white text-[#4B4B4B]">
