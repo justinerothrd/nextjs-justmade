1a0d91c6e3d37c1f4773b270eecd413736f0d865
justinerothrd <justinerothrd@gmail.com>
2026-04-20 21:58:23 -0400

    Update page.tsx

--- app/college/[category]/page.tsx
+++ app/college/[category]/page.tsx
@@ -87,8 +87,7 @@ export default async function CollegeCategoryPage({
                 href={`/college/product/${product.slug}`}
                 className="group block"
               >
-                <div className="overflow-hidden rounded-[22px] border border-[#F0ECE6] bg-white transition duration-300 ease-out group-hover:shadow-[0_16px_36px_rgba(0,0,0,0.06)] sm:rounded-[26px]">
-                  <div className="flex h-[260px] items-center justify-center p-4 sm:h-[320px]">
+                <div className="flex h-[260px] items-center justify-center p-4 sm:h-[320px]">
   <img
     src={product.images[0]}
     alt={product.name}
