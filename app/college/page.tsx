3c1c539b41b7055fd4e14811078f24300f318317
justinerothrd <justinerothrd@gmail.com>
2026-04-20 21:59:32 -0400

    Update page.tsx

--- app/college/page.tsx
+++ app/college/page.tsx
@@ -58,8 +58,8 @@ export default function CollegePage() {
                 <div className="overflow-hidden rounded-[22px] border border-[#F0ECE6] bg-white transition duration-300 ease-out group-hover:shadow-[0_16px_36px_rgba(0,0,0,0.06)] sm:rounded-[26px]">
                   <div className="flex h-[260px] items-center justify-center p-4 sm:h-[320px]">
   <img
-    src={product.images[0]}
-    alt={product.name}
+    src={category.image}
+    alt={category.title}
     className="h-[90%] w-[90%] object-contain transition duration-500 ease-out group-hover:scale-[1.03]"
   />
 </div>
