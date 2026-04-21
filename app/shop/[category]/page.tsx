92d25c5c1db416b0633203b05f686beab98af054
justinerothrd <justinerothrd@gmail.com>
2026-04-20 22:07:49 -0400

    Update page.tsx

--- app/shop/[category]/page.tsx
+++ app/shop/[category]/page.tsx
@@ -156,13 +156,13 @@ export default async function CategoryPage({
                 className="group block transition duration-300 ease-out hover:-translate-y-[2px]"
               >
                 <div className="overflow-hidden rounded-[22px] border border-[#F0ECE6] bg-white transition duration-300 ease-out group-hover:shadow-[0_16px_36px_rgba(0,0,0,0.06)] sm:rounded-[26px]">
-                  <div className="flex h-[260px] items-center justify-center p-4 sm:h-[320px]">
-  <img
-    src={product.images[0]}
-    alt={product.name}
-    className="h-[90%] w-[90%] object-contain transition duration-500 ease-out group-hover:scale-[1.03]"
-  />
-</div>
+                  <div className="flex h-[260px] items-center justify-center p-3 sm:h-[320px] sm:p-4">
+                    <img
+                      src={style.image}
+                      alt={style.title}
+                      className={`${style.imageClassName} object-contain transition duration-500 ease-out group-hover:scale-[1.02]`}
+                    />
+                  </div>
                 </div>
 
                 <div className="mt-3 text-center">
