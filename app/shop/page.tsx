d91102c1d63391f3ce60d24fa351d856aff99e0a
justinerothrd <justinerothrd@gmail.com>
2026-04-21 13:56:17 -0400

    Update page.tsx

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -27,7 +27,7 @@ export default function ShopPage() {
       <img
         src={category.image}
         alt={category.title}
-        className="h-full w-full object-contain transition-transform duration-500 ease-[cubic-bezier(0.22,1,0.36,1)] group-hover:scale-[1.08]"
+        className="h-full w-full object-contain transition-transform duration-500 ease-[cubic-bezier(0.22,1,0.36,1)] group-hover:scale-[1.07]"
       />
     </div>
   </div>
