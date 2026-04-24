9320916012c8f673947fad4bf7e5d6b36be052cd
justinerothrd <justinerothrd@gmail.com>
2026-04-24 15:12:18 -0400

    Update logos.ts

--- app/data/logos.ts
+++ app/data/logos.ts
@@ -9,15 +9,71 @@ export type Logo = {
 
 export const logos: Logo[] = [
 
-  // TYLER HILL
-  {
-    slug: "tyler-hill-varsity-arch",
-    name: "Varsity Arch",
-    image: "/logos/THCvarsityarch.png",
-    category: "Camp",
-    group: "Tyler Hill",
-    style: "Varsity",
-  },
+  // CAMP — TYLER HILL
+{
+  slug: "thc-country-club",
+  name: "Country Club",
+  image: "/logos/THC-countryclub.png",
+  category: "Camp",
+  group: "Tyler Hill",
+  style: "Classic",
+},
+{
+  slug: "thc-essentials",
+  name: "Essentials",
+  image: "/logos/THC-essentials.png",
+  category: "Camp",
+  group: "Tyler Hill",
+  style: "Minimal",
+},
+{
+  slug: "thc-script",
+  name: "Script",
+  image: "/logos/THC-script.png",
+  category: "Camp",
+  group: "Tyler Hill",
+  style: "Script",
+},
+{
+  slug: "thc-sporty-bear",
+  name: "Sporty Bear",
+  image: "/logos/THC-sportybear.png",
+  category: "Camp",
+  group: "Tyler Hill",
+  style: "Icon",
+},
+{
+  slug: "thc-varsity-arch-date",
+  name: "Varsity Arch with Date",
+  image: "/logos/THC-varsityarch-date.png",
+  category: "Camp",
+  group: "Tyler Hill",
+  style: "Varsity",
+},
+{
+  slug: "thc-varsity-arch",
+  name: "Varsity Arch",
+  image: "/logos/THC-varsityarch.png",
+  category: "Camp",
+  group: "Tyler Hill",
+  style: "Varsity",
+},
+{
+  slug: "thc-hotel",
+  name: "Hotel",
+  image: "/logos/thc-hotel.png",
+  category: "Camp",
+  group: "Tyler Hill",
+  style: "Classic",
+},
+{
+  slug: "thc-running-club",
+  name: "Running Club",
+  image: "/logos/thc-runningclub.png",
+  category: "Camp",
+  group: "Tyler Hill",
+  style: "Classic",
+},
 
   // PONTIAC
   {
