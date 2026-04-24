6d8c92a7452badd38551fb432a242421701d99c3
justinerothrd <justinerothrd@gmail.com>
2026-04-24 12:54:06 -0400

    Update page.tsx

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -259,6 +259,7 @@ export default function ProductPage() {
   logos={campLogos}
   selectedLogo={selectedLogo}
   onSelectLogo={setSelectedLogo}
+  defaultGroup="Tyler Hill"
 />
  <p className="mt-4 text-sm text-[#8A8178]">
   Don’t see your camp? Add it above.
