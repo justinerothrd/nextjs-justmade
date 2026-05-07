30ae6ffc1bd8724d6420ddbc21603a3422414db6
justinerothrd <justinerothrd@gmail.com>
2026-05-06 22:06:04 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,29 +5,7 @@ import { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
 import { Josefin_Sans } from "next/font/google";
 import MiniCart from "./components/MiniCart";
-export const metadata = {
-  
-  title: "Just Made Custom",
-  description: "Personalized gear for camp, college, and everyday.",
-  icons: {
-    icon: "/icon.png",
-  },
-  openGraph: {
-    title: "Just Made Custom",
-    description: "Personalized gear for camp, college, and everyday.",
-    url: "https://www.justmadecustom.com",
-    siteName: "Just Made Custom",
-    images: [
-      {
-        url: "/og-image.png",
-        width: 1200,
-        height: 630,
-        alt: "Just Made Custom",
-      },
-    ],
-    type: "website",
-  },
-};
+
 const josefin = Josefin_Sans({
   subsets: ["latin"],
   weight: ["300", "400", "600"],
